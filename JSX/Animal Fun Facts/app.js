0e096a383b131d2c63a1314b6bfc877d0384738f
Mohammad Ashiqur Rahman <ashiqurrussel2@gmail.com>
2020-12-28 00:11:01 +0100

    added background and some animals

--- JSX/Animal Fun Facts/app.js
+++ JSX/Animal Fun Facts/app.js
@@ -1,1 +1,46 @@
-import { animal } from './animals';
+import { animals } from './animals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+const title = "";
+
+const background = (
+    <img
+        className='background'
+        alt='ocean'
+        src='/images/ocean.jpg'
+    />
+)
+
+const images = [];
+
+for (const animal in animals) {
+    images.push(
+        <img
+            key={animal}
+            className='animal'
+            alt={animal}
+            src={animals[animal].image}
+            arialabel={animal}
+            role='button'
+        />
+    )
+}
+
+
+const animalFacts = (
+    <div>
+        <h1>
+            {title == "" ? "Click an animal for a fun       fact" : title}
+        </h1>
+        {background}
+        <div className='animals'>
+            {images}
+        </div>
+    </div>
+);
+
+ReactDOM.render(animalFacts, document.getElementById("root"));
+
+

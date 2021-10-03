85377acc86e7a651de876bd1b166b87d5763ce88
ashiqurras <ashiqurrussel2@gmail.com>
2021-10-03 22:27:42 +0200

    SImple color flipper done

--- Color-Flipper/setup/app.js
+++ Color-Flipper/setup/app.js
@@ -1,1 +1,22 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click",function(){  
+    const randomNumber = getRandomNumber();
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+});
+
+
+// Function for Getting  Random  number
+
+function getRandomNumber(){
+    let color = Math.floor(Math.random() * colors.length);
+   // console.log(color);
+    return color;
+ }
+
+
+ 
--- Color-Flipper/setup/hex.js
+++ Color-Flipper/setup/hex.js
@@ -1,1 +1,16 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click",function(){
+    const randomNumber = getHex();
+    document.body.style.backgroundColor = hex[randomNumber];
+    color.textContent = hex[randomNumber];
+
+});
+
+function getHex(){
+    const randcolor = Math.floor(Math.random()* 16777215);
+    console.log(randcolor);
+}

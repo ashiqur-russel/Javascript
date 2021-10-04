1e11c5b72f57ba2f97733667835519c24da4eee0
ashiqurras <ashiqurrussel2@gmail.com>
2021-10-05 01:16:35 +0200

    Implemented all functinality

---
+++ counter/app.js
@@ -0,0 +1,29 @@
+const btn_increase = document.getElementById("btn3");
+const btn_reset = document.getElementById("btn2");
+const btn_decrease = document.getElementById("btn1");
+
+const value = document.querySelector(".value"); 
+
+
+let counter = null;
+
+//Event listener for Increase Value
+btn_increase.addEventListener("click",function(){
+        counter++;
+        value.innerHTML = counter;
+})
+
+//Event listener for Decrease Value
+btn_decrease.addEventListener("click",function(){
+    counter--;
+    value.innerHTML = counter;
+})
+//Event listener for Reset Value
+btn_reset.addEventListener("click",function(){
+    counter = 0;
+    value.innerHTML = counter;
+})
+
+
+
+

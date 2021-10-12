77c84c9deac963f47b59fbbd98d258e1191221fb
ashiqurras <ashiqurrussel2@gmail.com>
2021-10-12 23:07:38 +0200

    save button  functionality

--- counter-p/index.js
+++ counter-p/index.js
@@ -5,8 +5,12 @@ let counter = 0;
 
 const increase = document.getElementById("increase");
 const decrease = document.getElementById("decrease");
+const save     = document.getElementById("save-btn");
+const value    = document.getElementById("res");
+const value1    = document.getElementById("save");
 
-const value = document.getElementById("res");
+
+
 
 increase.addEventListener("click",function(){
     counter++;
@@ -18,6 +22,20 @@ decrease.addEventListener("click",function(){
     counter--;
     value.innerText= counter;
 })
+
+save.addEventListener("click",function(){
+
+    let countStr = counter + " - ";
+    console.log(countStr);
+    value1.innerHTML = countStr;
+
+
+
+
+
+
+})
+
 
 
 

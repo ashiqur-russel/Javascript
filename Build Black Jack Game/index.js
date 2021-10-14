f4253d9582dee4793a331aa61e25df6f97fae3a5
ashiqurras <ashiqurrussel2@gmail.com>
2021-10-15 01:46:47 +0200

    push new card to array

--- Build Black Jack Game/index.js
+++ Build Black Jack Game/index.js
@@ -10,16 +10,17 @@ let messageShow = document.getElementById("message-el");
 let sumShow = document.querySelector("#sum-el");
 let cardShow = document.querySelector("#cards-el");
 
-
+//Start Game
+function startGame(){
+renderGame();
+}
 function renderGame(){
 
-    sumShow.textContent = "Sum : "+sum;
-    cardShow.textContent = "Cards: "+cards[0]+" "+cards[1];
+    cardShow.textContent = "Card : "+cards[0]+" "+cards[1];
 
-    console.log("Game starts");
+    sumShow.textContent = "Sum : "+sum;
     if(sum<=20){
-        message = "Do you want to draw a new card again? ";
-        
+        message = "Do you want to draw a new card again? ";   
     }else if(sum === 21){
         message = "You have got BlackJack!";
         let hasBlackJack = true;
@@ -33,14 +34,14 @@ function renderGame(){
 
 
 function newCard(){
-    console.log("Drew a new card from the deck!");
-    let card = 7;
-    sum +=card;
-
+    let randomCard = [6,4,7];
+    var randomItem = randomCard[Math.floor(Math.random()*randomCard.length)];
+   // console.log(randomCard);
+    console.log(randomItem);
+    cards.push(randomItem);
+    console.log(cards);
+    sum +=randomItem;
     renderGame();
-
+    cardShow.textContent = "Cards : "+cards[0]+" "+cards[1]+" "+randomItem;
 }
 
-function startGame(){
-    renderGame();
-}

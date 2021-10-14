9e9b454d31aed90c8015882d119636ae2143b41a
ashiqurras <ashiqurrussel2@gmail.com>
2021-10-15 00:23:57 +0200

    array

---
+++ Build Black Jack Game/practice.js
@@ -0,0 +1,23 @@
+// Array
+
+let cards = [7,4];
+
+cards.push(6);
+
+console.log(cards);
+
+let message = [
+    "Hey, how is it going?",
+    "Great ! Thank you .",
+    "All good.Been working on portfolio lately"
+];
+
+let newMessage = "Same here!"
+
+// add one item to last of array 
+message.push(newMessage);
+
+console.log(message);
+// remove last item from array list
+let lastItem = message.pop();
+console.log(lastItem);

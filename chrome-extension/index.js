81583f7f4c0ffee2fe926185591fad3e8cb7199b
ashiqurras <ashiqurrussel2@gmail.com>
2021-10-23 23:53:30 +0200

    input and save

---
+++ chrome-extension/index.js
@@ -0,0 +1,12 @@
+
+let saveInput = document.querySelector(".res");
+let input_el = "";
+let input_Button = document.querySelector("#input-btn");
+console.log(input_el);
+input_Button.addEventListener("click",function(){
+    console.log("Button clicked for add event listener")
+    input_el = document.querySelector("input-el");
+    input_Button.textContent
+    console.log(input_el);
+
+})

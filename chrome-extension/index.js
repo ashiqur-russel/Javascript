d3e40bd1fe5b46a72ad9a5dfc2194ed111af07ec
ashiqurras <ashiqurrussel2@gmail.com>
2021-10-24 00:35:32 +0200

    function for add input value to list

--- chrome-extension/index.js
+++ chrome-extension/index.js
@@ -4,12 +4,26 @@
 // myLeads -> should be assigned to an empty array
 // inputEl -> should be assigned to the text input field
 
+
 let myLeads = []
+
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+
 
 inputBtn.addEventListener("click", function() {
         // Push the value from the inputEl into the myLeads array 
-        myLeads.push("www.awesomelead.com")
-        console.log(myLeads)
+        myLeads.push(inputEl.value)
+        renderLeads()
+
 })
+
+function renderLeads() {
+    // Assign it to an empty string to begin with
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += "<li>" + myLeads[i] + "</li>"
+    }
+    ulEl.innerHTML = listItems  
+}

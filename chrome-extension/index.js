c09a87a300fe1d14529fbf50a5b5ab5818cc93ce
ashiqurras <ashiqurrussel2@gmail.com>
2021-10-24 00:12:12 +0200

    when to use let and const

--- chrome-extension/index.js
+++ chrome-extension/index.js
@@ -1,12 +1,12 @@
 
-let saveInput = document.querySelector(".res");
-let input_el = "";
-let input_Button = document.querySelector("#input-btn");
-console.log(input_el);
-input_Button.addEventListener("click",function(){
+
+// Create two variables:
+// myLeads -> should be assigned to an empty array
+// inputEl -> should be assigned to the text input field
+
+let inputBtn = document.getElementById("input-btn")
+inputBtn.addEventListener("click", function() {
     console.log("Button clicked for add event listener")
-    input_el = document.querySelector("input-el");
-    input_Button.textContent
-    console.log(input_el);
+    
 
 })

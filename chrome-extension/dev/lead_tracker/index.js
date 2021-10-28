d3c979d68f8dcc10a09197a8dd9d2477db026aa0
ashiqurras <ashiqurrussel2@gmail.com>
2021-10-28 04:19:00 +0200

    save input data to local storage

--- chrome-extension/dev/lead_tracker/index.js
+++ chrome-extension/dev/lead_tracker/index.js
@@ -4,20 +4,34 @@
 // myLeads -> should be assigned to an empty array
 // inputEl -> should be assigned to the text input field
 
+let myLeads = [];
 
-let myLeads = []
+/* let myLeads =`["www.facebook.com"]`
+myLeads = JSON.parse(myLeads);
+myLeads.push("www.hello.com");
+myLeads =JSON.stringify(myLeads);
+console.log(typeof myLeads); */
 
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
+//Local storage
+//localStorage.setItem("myName","Ashiqur Russel")
+
+// let Myname = localStorage.getItem("myName");
+// console.log(Myname)
+
 
 inputBtn.addEventListener("click", function() {
         // Push the value from the inputEl into the myLeads array 
         myLeads.push(inputEl.value)
-        //clear the input
         inputEl.value = ""
+        // save the myLeads array to local storage and turn string
+        localStorage.setItem("myLeads",JSON.stringify(myLeads));
+        //clear the input
         renderLeads()
+        console.log(localStorage.getItem("myLeads"))
 
 })
 

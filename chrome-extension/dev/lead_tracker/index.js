a66b7b2c19071d86948a14e9f0689aedf93607b4
ashiqurras <ashiqurrussel2@gmail.com>
2021-10-29 17:03:20 +0200

    more dynamic the function

--- chrome-extension/dev/lead_tracker/index.js
+++ chrome-extension/dev/lead_tracker/index.js
@@ -17,32 +17,31 @@ const dataFromStorage = JSON.parse(localStorage.getItem("myLeads"));
 
 if(dataFromStorage){                                                 // if there is data fetch and show
     myLeads = dataFromStorage
-    renderLeads(myLeads)
+    render()
 }
 
 inputBtn.addEventListener("click", function() {
         myLeads.push(inputEl.value)                                  // Push the value from the inputEl into the myLeads array 
         inputEl.value = ""
         localStorage.setItem("myLeads",JSON.stringify(myLeads));     // save the myLeads array to local storage and turn string
-        renderLeads()                                                //clear the input
+        render()                                                //clear the input
         console.log(localStorage.getItem("myLeads"))
 })
 
 deleteBtn.addEventListener("click",function(){
     localStorage.clear();
     myLeads =[];
-    renderLeads(myLeads);
+    render();
 })
 
-
-function renderLeads(leads) {
+function render() {
     // Assign it to an empty string to begin with
     let listItems = ""
-    for (let i = 0; i < leads.length; i++) {
+    for (let i = 0; i < myLeads.length; i++) {
         listItems += `
             <li>
-                <a target='_blank' href='${leads[i]}'>
-                    ${leads[i]}
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
                 </a>
             </li>
         `

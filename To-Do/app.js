2279f0afbbdfd59e4f51dfc0b64d5f86ce76aa65
ashiqurras <ashiqurrussel2@gmail.com>
2021-11-15 19:29:58 +0100

    todo functionality js

---
+++ To-Do/app.js
@@ -0,0 +1,40 @@
+//Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+//Event Listener
+todoButton.addEventListener('click',addTodo);
+
+
+//Functions
+function addTodo(event){
+    //Prevent form from submitting
+    event.preventDefault();
+    //Todo div
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add("todo");
+    //Create li
+    const newTodo = document.createElement('li');
+    newTodo.innerText='hey';
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+
+    //check Mark button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML='<i class=" fa fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+
+    //check Trash button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML='<i class=" fa fa-trash"></i>';
+    trashButton.classList.add("complete-btn");
+    todoDiv.appendChild(trashButton);
+
+    //append Todo List
+    todoList.appendChild(todoDiv);
+
+}
+
+

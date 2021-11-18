cfe838ea9e73b984d787c7921d89f5cf9e31a8bd
ashiqurras <ashiqurrussel2@gmail.com>
2021-11-18 14:02:58 +0100

    style

--- To-Do/app.js
+++ To-Do/app.js
@@ -47,6 +47,12 @@ function deleteCheck(e){
         const todo = item.parentElement;
         todo.remove();
     }
+    //Check Mark
+    if(item.classList[0]==='complete-btn'){
+        const todo = item.parentElement;
+        todo.classList.toggle("completed");
+
+    }
 }
 
 
